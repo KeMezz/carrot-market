ed715637af6caf47f5e13dcfd444326b1df58c3f
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-11-03 00:13:19 +0900

    tailwind css practice 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,29 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-black-500">it works!</h1>
-    </div>
+    <section className="bg-slate-400 py-20 px-10 flex flex-col space-y-10">
+      <div className="bg-white p-8 rounded-2xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Tooly Table</span>
+          <span className="font-semibold">$21</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$40</span>
+        </div>
+        <div className="mt-5 mx-auto bg-blue-500 p-3 text-white rounded-xl w-2/3 self-center text-center">
+          Checkout
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+    </section>
   );
 };
 
