e55bfe1b7759b788a09b5b855ae089877c8e76f1
KeMezz <pnescene@icloud.com>
2022-11-16 23:28:07 +0900

    little design fixes

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -10,7 +10,11 @@ function Enter() {
   const onPhoneClick = () => setMethod("phone");
   return (
     <main className="mt-16 px-4">
-      <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
+      <h3 className="text-3xl font-bold text-center">
+        🥕
+        <br />
+        Enter to Carrot
+      </h3>
       <section className="mt-16">
         <div className="flex flex-col items-center">
           <h5 className="text-sm text-gray-500 font-medium">Enter using:</h5>
@@ -58,9 +62,9 @@ function Enter() {
                   +82
                 </span>
                 <input
-                  type="number"
+                  type="phone"
                   required
-                  className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-r-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-400 focus:border-orange-400"
+                  className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-r-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-400 focus:border-orange-400"
                 />
               </div>
             ) : null}
