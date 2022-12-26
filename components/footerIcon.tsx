9d926a8f6a316064341d4f36b0d559a36289e066
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-12-26 19:59:33 +0900

    setup layout

---
+++ components/footerIcon.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from "next";
+
+interface FooterIconProps {
+  d: string;
+  name: string;
+}
+
+const FooterIcon: NextPage<FooterIconProps> = ({ d, name }) => {
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d={d} />
+      </svg>
+      <p className="text-sm">{name}</p>
+    </div>
+  );
+};
+
+export default FooterIcon;
