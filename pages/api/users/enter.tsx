6eb828eb04298ebafaebd6364915552d0d7d616f
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-04-10 21:47:25 +0900

    Update enter.tsx

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,21 @@ import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  let user;
+  if (email) {
+    user = await client.user.findUnique({ where: { email } });
+    if (!user) {
+      console.log("Did not find. Will create.");
+      user = await client.user.create({ data: { name: "Anonymous", email } });
+    }
+    console.log(user);
+  } else if (phone) {
+    user = await client.user.findUnique({ where: { phone } });
+    if (!user) {
+      user = await client.user.create({ data: { name: "Anonymous", phone } });
+    }
+  }
   return res.json({ ok: true });
 }
 
