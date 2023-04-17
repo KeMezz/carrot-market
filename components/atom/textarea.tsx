166c3d2055aa22ed2fb92038d4f8897415c91c28
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-04-17 11:06:57 +0900

    register 추가

--- components/atom/textarea.tsx
+++ components/atom/textarea.tsx
@@ -1,18 +1,26 @@
 import { NextPage } from "next";
 import InputLabel from "./input-label";
+import { UseFormRegisterReturn } from "react-hook-form";
 
 interface TextareaProps {
   name?: string;
   id: string;
   placeholder?: string;
+  register: UseFormRegisterReturn;
 }
 
-const Textarea: NextPage<TextareaProps> = ({ name, id, placeholder }) => {
+const Textarea: NextPage<TextareaProps> = ({
+  name,
+  id,
+  placeholder,
+  register,
+}) => {
   return (
     <div className="flex flex-col gap-2">
       {name ? <InputLabel id={id} name={name} /> : null}
       <div className="flex items-center">
         <textarea
+          {...register}
           id={id}
           rows={4}
           placeholder={placeholder}
--- pages/streams/upload.tsx
+++ pages/streams/upload.tsx
@@ -3,13 +3,25 @@ import FilledBtn from "@components/atom/filled-btn";
 import Textarea from "@components/atom/textarea";
 import TextInput from "@components/atom/text-input";
 import Layout from "@components/template/layout";
+import { useForm } from "react-hook-form";
 
 const UploadStream: NextPage = () => {
+  const { register } = useForm();
   return (
     <Layout canGoBack>
       <section className="flex flex-col gap-4 p-4 mt-8">
-        <TextInput name="상품명" id="name" />
-        <TextInput name="가격" id="price" />
+        <TextInput
+          name="상품명"
+          id="name"
+          type="text"
+          register={register("name")}
+        />
+        <TextInput
+          name="가격"
+          id="price"
+          type="number"
+          register={register("price")}
+        />
         <Textarea name="설명" id="description" />
         <FilledBtn title="Go live" />
       </section>
