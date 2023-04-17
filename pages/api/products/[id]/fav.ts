07be83c0800ac575720c6f742c05c19546cad945
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-04-17 18:04:34 +0900

    서버를 통해 좋아요 표시

---
+++ pages/api/products/[id]/fav.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+  const alreadyExists = await client.fav.findFirst({
+    where: {
+      productId: Number(id),
+      userId: user?.id,
+    },
+  });
+  if (alreadyExists) {
+    await client.fav.delete({
+      where: {
+        id: alreadyExists.id,
+      },
+    });
+    return res.status(204).json({ success: true });
+  } else {
+    await client.fav.create({
+      data: {
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+        product: {
+          connect: {
+            id: Number(id),
+          },
+        },
+      },
+    });
+    return res.status(201).json({ success: true });
+  }
+}
+
+export default withApiSession(
+  withHandler({ methods: ["POST"], handlerFn: handler })
+);
