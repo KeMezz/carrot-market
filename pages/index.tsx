4c3c6daf811aa30186a0a54920f146df11ddfa42
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-04-17 11:42:46 +0900

    get products with SWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,31 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import FloatingBtn from "@components/atom/floating-btn";
 import Layout from "@components/template/layout";
-import Product from "@components/molecule/product";
+import ProductCard from "@components/molecule/product";
 import useUser from "@libs/client/useUser";
+import useSWR from "swr";
+import { Product } from "@prisma/client";
+
+interface ProductsResponse {
+  success: boolean;
+  products: Product[];
+}
 
 const Home: NextPage = () => {
   const router = useRouter();
   const goToUpload = () => router.push("/products/upload");
   const { user, isLoading } = useUser();
+  const { data } = useSWR<ProductsResponse>("/api/products");
+  console.log(data);
   return (
     <Layout title="홈">
       <div className="divide-y">
-        {Array.from({ length: 10 }, (_, i) => i).map((i) => (
-          <Product
-            key={i}
-            title="iPhone 14 Pro Max"
+        {data?.products.map((product) => (
+          <ProductCard
+            key={product.id}
+            title={product.name}
             color="Black"
-            price={999}
+            price={product.price}
             heart={1}
             comment={1}
           />
