589205380558562a4595062d1be004c988c9b402
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-04-17 11:07:13 +0900

    product upload 백엔드 코드 작성

---
+++ pages/api/products/index.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    body: { name, price, description },
+    session: { user },
+  } = req;
+  const product = await client.product.create({
+    data: {
+      name,
+      price: +price,
+      description,
+      image: "",
+      user: { connect: { id: user?.id } },
+    },
+  });
+  return res.json({ success: true, product });
+}
+
+export default withApiSession(
+  withHandler({ method: "POST", handlerFn: handler })
+);
