3385480d0871ac8b130725085891c6124c58f008
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-04-18 21:41:03 +0900

    add onClick function

--- components/atom/reaction-btn-big.tsx
+++ components/atom/reaction-btn-big.tsx
@@ -1,19 +1,34 @@
+import { cls } from "@libs/client/utils";
 import { NextPage } from "next";
 
 interface ReactionBtnProps {
   title: string;
   d: string;
   count: number;
+  isClicked?: boolean;
+  onClick?: () => void;
 }
 
-const ReactionBtnBig: NextPage<ReactionBtnProps> = ({ title, d, count }) => {
+const ReactionBtnBig: NextPage<ReactionBtnProps> = ({
+  title,
+  d,
+  count,
+  isClicked,
+  onClick,
+}) => {
   return (
-    <button className="flex gap-2 items-center">
+    <button
+      className={cls(
+        "flex gap-2 items-center",
+        isClicked ? "text-green-500" : ""
+      )}
+      onClick={onClick}
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        strokeWidth={1.5}
+        strokeWidth={1.8}
         stroke="currentColor"
         className="w-5 h-5"
       >
