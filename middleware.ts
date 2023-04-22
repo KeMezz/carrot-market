33ca76e98fda74ef07ca45d22b6c3f1b995d5de4
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-04-22 12:15:34 +0900

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import {
+  NextFetchEvent,
+  NextRequest,
+  NextResponse,
+  userAgent,
+} from "next/server";
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const ua = userAgent(req);
+  if (ua.isBot) {
+    return NextResponse.json({ message: "Bot Not Allowed" }, { status: 403 });
+  }
+  if (
+    !req.cookies.has("carrotsession") &&
+    !req.nextUrl.pathname.startsWith("/enter")
+  ) {
+    req.nextUrl.pathname = "/enter";
+    return NextResponse.redirect(req.nextUrl);
+  }
+}
