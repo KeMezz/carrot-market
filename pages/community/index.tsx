9d41c46c7f20f879c6a7198fc797e3cfe53bf6b2
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-02 14:41:50 +0900

    style refactor

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -35,22 +35,22 @@ const Community: NextPage = () => {
               <SkProductCard key={i} />
             ))}
         {data?.posts.length === 0 ? (
-          <div className="flex flex-col gap-4 justify-center items-center h-[85vh] text-gray-400">
+          <div className="flex flex-col gap-4 justify-center items-center min-h-[calc(100vh-152px)] text-gray-400">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="w-16 h-16"
+              className="w-20 h-20"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+                d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
               />
             </svg>
-            <h3>아직 동네에 올라온 글이 없어요</h3>
+            <h3>커뮤니티의 첫 글을 작성해주세요!</h3>
           </div>
         ) : null}
         {data?.posts.map((post) => (
--- pages/index.tsx
+++ pages/index.tsx
@@ -32,12 +32,31 @@ const Home: NextPage = () => {
   }
   return (
     <Layout title="홈">
-      <div className={"flex flex-col divide-y"}>
+      <div className="flex flex-col divide-y min-h-[calc(100vh-152px)]">
         {!isLoading
           ? null
           : Array.from({ length: 4 }, (_, i) => i).map((i) => (
               <SkProductCard key={i} />
             ))}
+        {data?.products.length === 0 ? (
+          <div className="flex flex-col gap-4 justify-center items-center text-gray-400 m-auto">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-20 h-20"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+              />
+            </svg>
+            <p>첫 중고 거래글을 등록해보세요!</p>
+          </div>
+        ) : null}
         {data?.products.map((product) => (
           <ProductCard
             id={product.id}
