ade2b586ee13895237bbc4d3c93339f5b9a28dd8
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-07-16 19:42:51 +0900

    안쓰는 코드 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from "next";
-import FloatingBtn from "@components/atom/floating-btn";
 import Layout from "@components/template/layout";
 import ProductCard from "@components/molecule/product-card";
 import useUser from "@libs/client/useUser";
 import useSWR from "swr";
 import { Product } from "@prisma/client";
-import Link from "next/link";
 import Pagination from "@components/molecule/pagination";
 import { useRouter } from "next/router";
 import SkProductCard from "@components/skeleton/skeleton-product-card";
@@ -79,9 +77,6 @@ const Home: NextPage = () => {
           />
         </div>
       )}
-      {/* <Link href="/products/upload">
-        <FloatingBtn d="M12 4.5v15m7.5-7.5h-15" />
-      </Link> */}
     </Layout>
   );
 };
