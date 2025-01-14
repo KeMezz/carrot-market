d0190718e0b7627ffa8a278baa46d507f07ad112
정형진 <88423849+KeMezz@users.noreply.github.com>
2025-01-14 23:14:27 +0900

    스켈레톤 디자인 수정

--- components/skeleton/skeleton-product-card.tsx
+++ components/skeleton/skeleton-product-card.tsx
@@ -2,15 +2,16 @@ const SkProductCard = () => {
   return (
     <article className="flex p-4 gap-4 rounded-md">
       <div className="bg-slate-300 rounded-lg w-24 h-24 animate-pulse flex-shrink-0" />
-      <section className="flex flex-col justify-center gap-2 animate-pulse rounded-md">
-        <div className="w-24 h-6 bg-slate-300 animate-pulse rounded-md" />
-        <div className="w-52 h-4 bg-slate-300 animate-pulse rounded-md" />
-        <div className="w-56 h-4 bg-slate-300 rounded-md" />
-      </section>
-      <section className="flex gap-2 mt-auto ml-auto">
-        <div className="w-8 h-4 bg-slate-300 animate-pulse rounded-md" />
-        <div className="w-8 h-4 bg-slate-300 animate-pulse rounded-md" />
-      </section>
+      <div className="flex flex-col justify-between w-full">
+        <section className="flex flex-col justify-center mt-5 gap-2 animate-pulse rounded-md">
+          <div className="w-48 h-5 bg-slate-300 animate-pulse rounded-md" />
+          <div className="w-20 h-4 bg-slate-300 animate-pulse rounded-md" />
+        </section>
+        <section className="flex gap-2 mt-auto ml-auto">
+          <div className="w-8 h-4 bg-slate-300 animate-pulse rounded-md" />
+          <div className="w-8 h-4 bg-slate-300 animate-pulse rounded-md" />
+        </section>
+      </div>
     </article>
   );
 };
